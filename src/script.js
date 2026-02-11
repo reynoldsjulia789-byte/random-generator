7f0a8616c8e7db31a6f301536cd32478a32e3d6b
Julia <reynolds.julia789@gmail.com>
2026-02-11 18:53:44 +0000

    added randomized background gradients

--- src/script.js
+++ src/script.js
@@ -1,11 +1,87 @@
 console.log('👍 JS Connected');
 
-// Tailwind Background Utlities
-const backgrounds =
+// Taiwind gradient direction
+const bgDirection = 
 [
-  "bg-red-300",
-  "bg-green-300",
-  "bg-blue-300"
+  "bg-gradient-to-t",
+  "bg-gradient-to-tr",
+  "bg-gradient-to-r",
+  "bg-gradient-to-br",
+  "bg-gradient-to-b",
+  "bg-gradient-to-bl",
+  "bg-gradient-to-l",
+  "bg-gradient-to-tl",
+  "bg-radial",
+  "bg-radial-[at_50%_100%]",
+  "bg-radial-[at_100%_100%]",
+  "bg-radial-[at_100%_0%]",
+  "bg-radial-[at_50%_100%]",
+  "bg-radial-[at_0%_100%]",
+  "bg-radial-[at_0%_0%]"
+];
+
+// Tailwind Background Utlities
+const startColors =
+[
+  "from-red-500",
+  "from-orange-500",
+  "from-amber-500",
+  "from-yellow-500",
+  "from-lime-500",
+  "from-green-500",
+  "from-emerald-500",
+  "from-teal-500",
+  "from-cyan-500",
+  "from-sky-500",
+  "from-blue-500",
+  "from-indigo-500",
+  "from-violet-500",
+  "from-purple-500",
+  "from-fuchsia-500",
+  "from-pink-500",
+  "from-rose-500"
+];
+
+const middleColors =
+[
+  "via-red-300",
+  "via-orange-300",
+  "via-amber-300",
+  "via-yellow-300",
+  "via-lime-300",
+  "via-green-300",
+  "via-emerald-300",
+  "via-teal-300",
+  "via-cyan-300",
+  "via-sky-300",
+  "via-blue-300",
+  "via-indigo-300",
+  "via-violet-300",
+  "via-purple-300",
+  "via-fuchsia-300",
+  "via-pink-300",
+  "via-rose-300"
+];
+
+const endColors =
+[
+  "to-red-500",
+  "to-orange-500",
+  "to-amber-500",
+  "to-yellow-500",
+  "to-lime-500",
+  "to-green-500",
+  "to-emerald-500",
+  "to-teal-500",
+  "to-cyan-500",
+  "to-sky-500",
+  "to-blue-500",
+  "to-indigo-500",
+  "to-violet-500",
+  "to-purple-500",
+  "to-fuchsia-500",
+  "to-pink-500",
+  "to-rose-500"
 ];
 
 // Sample design dishes JSON data
@@ -97,12 +173,19 @@ function getRandomDish()
   dishText       .textContent = `${selectedDish.dish}`;
   dishIngredients.textContent = `${selectedDish.ingredients}`;
 
-  // Get random background
-  const randomBgIndex = Math.floor(Math.random() * backgrounds.length);
-  const newBackground = backgrounds[randomBgIndex];
+  // Get random background color and gradient direction
+  const randomGradientIdx = Math.floor(Math.random() * bgDirection.length);
+  const randomBgIndex1    = Math.floor(Math.random() * startColors.length);
+  const randomBgIndex2    = Math.floor(Math.random() * middleColors.length);
+  const randomBgIndex3    = Math.floor(Math.random() * endColors.length);
+
+  const gradientDirection = bgDirection[randomGradientIdx];
+  const backgroundColor1  = startColors[randomBgIndex1];
+  const backgroundColor2  = middleColors[randomBgIndex2];
+  const backgroundColor3  = endColors[randomBgIndex3];
 
   // Replace entire class attribute
-  body.className = `${newBackground} min-h-screen flex items-center justify-center transition-all duration-700`;
+  body.className = `${gradientDirection} ${backgroundColor1} ${backgroundColor2} ${backgroundColor3} min-h-screen flex items-center justify-center transition-all duration-700`;
 }
 
 // Add event listener to button
